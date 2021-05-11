3163f761dfb915bcaac174532a6e48ebb4ed4c68
Facundo La Rocca <facundo_larocca@yahoo.com.ar>
2021-05-11 10:05:51 -0300

    Initial commit

---
+++ FacuTheRock.Talks.Azure.WebPubSub.Consumer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Websocket.Client;
+
+namespace FacuTheRock.Talks.Azure.WebPubSub.Consumer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Azure Days Latinoamierca 2021");
+            Console.WriteLine("Welcome to the Azure Web PubSub - Consumer\n\n");
+
+            using var client = new WebsocketClient(new Uri("<Client URI>"));
+
+            using var _ = client.MessageReceived
+                .Subscribe(msg => Console.WriteLine($"Message received: {msg}"));
+
+            await client.Start();
+
+            Console.WriteLine("Connected!!!");
+            Console.Read();
+        }
+    }
+}
---
+++ FacuTheRock.Talks.Azure.WebPubSub.Publisher/Program.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Messaging.WebPubSub;
+using System;
+using System.Threading.Tasks;
+
+namespace FacuTheRock.Talks.Azure.WebPubSub.Publisher
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Azure Days Latinoamierca 2021");
+            Console.WriteLine("Welcome to the Azure Web PubSub - Producer\n\n");
+
+            var serviceClient = new WebPubSubServiceClient("<Connection string>", "<Hub>");
+
+            string message;
+            while ((message = ReadInput()) != "c")
+            {
+                await serviceClient.SendToAllAsync(message);
+            }
+        }
+
+        private static string ReadInput()
+        {
+            Console.Write("Enter a message or press c to exit: ");
+            return Console.ReadLine();
+        }
+    }
+}
